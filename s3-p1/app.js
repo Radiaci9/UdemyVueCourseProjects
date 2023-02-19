a7bb14d9760a0ac7faa838ab0aadf3c1794f9da1
Radiaci9 <cvirko1998@mail.ru>
2023-02-19 19:04:51 +0100

    [Section 3 - Project 1] - Completed: Rendering conditional content & lists

--- s3-p1/app.js
+++ s3-p1/app.js
@@ -1,7 +1,23 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { goals: [], enteredGoalValue: '' };
   },
+  computed: {
+    isListEmpty() {
+      return this.goals.length === 0;
+    }
+  },
+  methods: {
+    addGoal() {
+      if (!this.enteredGoalValue) return;
+
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = ''
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    }
+  }
 });
 
 app.mount('#user-goals');
