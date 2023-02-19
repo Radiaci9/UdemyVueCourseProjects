1028f2b7800759caaf4ca3062444769fe6f182ad
Radiaci9 <cvirko1998@mail.ru>
2023-02-19 03:48:17 +0100

    [Section 2 - Project 2] - Completed

--- s2-p2/app.js
+++ s2-p2/app.js
@@ -1,9 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 50,
+      name: '',
+      confirmedName: '',
     };
   },
+  methods: {
+    confirmInput(){
+      this.confirmedName = this.name;
+    },
+    submitForm1(event) {
+      alert('Submitt')
+    },
+    submitForm2(event) {
+      event.preventDefault();
+
+      alert('Submitt')
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num = 1) {
+      this.counter += num;
+    },
+    reduce(num = 1) {
+      this.counter -= num;
+    }
+  }
 });
 
 app.mount('#events');
