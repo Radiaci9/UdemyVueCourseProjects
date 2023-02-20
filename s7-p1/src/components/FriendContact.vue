6b277ad3f959e58bf05f96d47cec7ca08355f3cb
Radiaci9 <cvirko1998@mail.ru>
2023-02-20 08:00:54 +0100

    [Section 7 - Project 1] - Completed: Vue CLI

---
+++ s7-p1/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        detailsAreVisible: false,
+      }
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      },
+    },
+    props: ['friend']
+  }
+</script>
