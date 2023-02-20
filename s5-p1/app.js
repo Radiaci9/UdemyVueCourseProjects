af9f5da778631268cac0d03589216ae12dc59cf8
Radiaci9 <cvirko1998@mail.ru>
2023-02-20 04:40:25 +0100

    [Section 5 - Project 1] - Completed: Vue app lifecycle, multipleApps

--- s5-p1/app.js
+++ s5-p1/app.js
@@ -5,14 +5,56 @@ const app = Vue.createApp({
       message: 'Vue is great!',
     };
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
   methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
     },
   },
 });
 
 app.mount('#app');
+
+app.unmount();
+
+
+const app2 = Vue.createApp({
+  template: `
+    <h2>App 2</h2>
+    <p>{{ message2 }}</p>
+  `,
+  data() {
+    return {
+      message2: 'Vue is great!',
+    };
+  },
+});
+
+app2.mount('#app2');
