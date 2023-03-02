6092383d16ea99ed6cee20bbbd7661962e6a7f5b
Radiaci9 <cvirko1998@mail.ru>
2023-03-03 00:44:46 +0100

    [Section 9 - Project 1] InProgress: add slots

--- s9-p1/src/App.vue
+++ s9-p1/src/App.vue
@@ -1,35 +1,49 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+	<div>
+		<TheHeader />
+		<badge-list></badge-list>
+		<user-info
+			:full-name="activeUser.name"
+			:info-text="activeUser.description"
+			:role="activeUser.role"
+		></user-info>
+		<CourseGoals #default="slotProps">
+			<h2>{{ slotProps.goal }}</h2>
+		</CourseGoals>
+	</div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
 export default {
-  data() {
-    return {
-      activeUser: {
-        name: 'Rad',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
+	components: {
+		TheHeader,
+		BadgeList,
+		UserInfo,
+		CourseGoals,
+	},
+	data() {
+		return {
+			activeUser: {
+				name: 'Rad',
+				description: 'Site owner and admin',
+				role: 'admin',
+			},
+		};
+	},
 };
 </script>
 
 <style>
 html {
-  font-family: sans-serif;
+	font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
