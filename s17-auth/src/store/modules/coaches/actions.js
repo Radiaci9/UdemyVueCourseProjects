742808deea9bf468af56b9962da20d941647b2e9
Radiaci9 <cvirko1998@mail.ru>
2023-04-10 23:26:52 +0200

    [Section 17 - Auth] Completed

--- s17-auth/src/store/modules/coaches/actions.js
+++ s17-auth/src/store/modules/coaches/actions.js
@@ -6,14 +6,17 @@ export default {
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
-      areas: data.areas
+      areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches/${userId}.json`,
+      `https://s12-http-requests-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
-        body: JSON.stringify(coachData)
+        body: JSON.stringify(coachData),
       }
     );
 
@@ -25,7 +28,7 @@ export default {
 
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
+      id: userId,
     });
   },
   async loadCoaches(context, payload) {
@@ -34,7 +37,7 @@ export default {
     }
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches.json`
+      `https://s12-http-requests-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
     );
     const responseData = await response.json();
 
@@ -52,12 +55,12 @@ export default {
         lastName: responseData[key].lastName,
         description: responseData[key].description,
         hourlyRate: responseData[key].hourlyRate,
-        areas: responseData[key].areas
+        areas: responseData[key].areas,
       };
       coaches.push(coach);
     }
 
     context.commit('setCoaches', coaches);
     context.commit('setFetchTimestamp');
-  }
+  },
 };
