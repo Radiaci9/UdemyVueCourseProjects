7c7c3fac7bdaf19d908fe22cce438c898ee37cc1
Radiaci9 <cvirko1998@mail.ru>
2023-04-11 17:19:05 +0200

    [Section 19 - Project 1] Completed: Basics Composition API

--- s19-p1/src/App.vue
+++ s19-p1/src/App.vue
@@ -1,14 +1,70 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="setNewAge(18)">Change age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Last Name" ref="lastNameRef" />
+      <button @click="setNewLastName">Set lastName</button>
+      <h4>{{ fullName }}</h4>
+    </div>
+    <br />
+    <UserData :user-name="user.name" :user-age="user.age" />
   </section>
 </template>
 
 <script>
+import { ref, reactive, computed, watch, provide } from 'vue';
+import UserData from './components/UserData.vue';
+
 export default {
-  data() {
+  components: {
+    UserData,
+  },
+  setup() {
+    const firstName = ref('');
+    const lastName = ref('');
+    const ageeeeeee = ref(11);
+    const lastNameRef = ref(null);
+    const user = reactive({
+      name: 'Rad',
+      age: 25,
+    });
+
+    setTimeout(() => {
+      user.name = 'Rad9';
+      user.age = 33;
+      ageeeeeee.value = 2222;
+    }, 2000);
+
+    const setNewAge = (newAge) => {
+      user.age = newAge;
+      ageeeeeee.value = newAge;
+    };
+    const setFirstName = (event) => (firstName.value = event.target.value);
+    const setNewLastName = () => (lastName.value = lastNameRef.value.value);
+
+    const fullName = computed(() => `${firstName.value} ${lastName.value}`);
+
+    watch([lastName, firstName], (newVal, oldValue) => {
+      user.age += 1;
+
+      console.log('🚀 ~ file: App.vue:37 ~ watch ~ newVal:', newVal);
+      console.log('🚀 ~ file: App.vue:37 ~ watch ~ newVal:', oldValue);
+    });
+
+    provide('userAge', ageeeeeee);
+
     return {
-      userName: 'Rad',
+      user,
+      setNewAge,
+      fullName,
+      setFirstName,
+      lastName,
+      lastNameRef,
+      setNewLastName,
     };
   },
 };
---
+++ s19-p1/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>{{ userName }}</h3>
+  <h3>{{ userAge }}</h3>
+  <h4>{{ mix }}</h4>
+</template>
+
+<script>
+import { computed, inject } from 'vue';
+
+export default {
+  setup(props) {
+    const userAge = inject('userAge');
+    console.log(userAge);
+    const mix = computed(() => props.userName + '//' + userAge.value);
+
+    // context.emit(); //
+    return { mix, userAge };
+  },
+  props: ['userName'],
+  // computed: {
+  //   mix() {
+  //     return this.userName + this.userAge;
+  //   },
+  // },
+};
+</script>
